5148df443284b8c7f7b70e2a712b06c138e0dba3
tictactoe-test <vittoriodequec@gmail.com>
2026-02-14 16:45:09 +0100

    style : add display cli

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,13 +1,5 @@
-from tictactoe.game.game_manager import GameManager
+from tictactoe.ui.cli import TicTacToeCLI
 
-gm = GameManager(3)
-gm.start_game("X")
-
-gm.make_move(0,0)
-gm.make_move()
-
-for row in gm.board.grid:
-    print(row)
-
-print("Current player:", gm.current_player)
-print("Game over:", gm.game_over)
+if __name__ == "__main__":
+    cli = TicTacToeCLI(size=3)
+    cli.play()
